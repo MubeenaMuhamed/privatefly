cd6567a421ae5519620eaf25ec498c62fb7091e1
mubeena <mubeena@qburst.com>
2017-09-18 11:46:47 +0530

    Code modification

--- src/main/java/com/privatefly/controller/HomeController.java
+++ src/main/java/com/privatefly/controller/HomeController.java
@@ -1,9 +1,5 @@
 package com.privatefly.controller;
 
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.List;
 
 import org.apache.log4j.Logger;
@@ -14,8 +10,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import com.privatefly.model.PrivateFlyDAO;
 import com.privatefly.model.PrivateFlyModel;
+import com.privatefly.service.PrivateflyService;
 
 @Controller
 public class HomeController {
@@ -23,14 +19,13 @@ public class HomeController {
 	private static final Logger logger = Logger.getLogger(HomeController.class);
 
 	@Autowired
-	private PrivateFlyDAO privateFlyDAO;
+	private PrivateflyService privateflyService;
 
 	@RequestMapping("/")
 	public String viewHome(ModelMap map) {
-		if (logger.isDebugEnabled()) {
-			logger.debug("viewHome is executed!");
-		}
-		List<PrivateFlyModel> aircrafts = privateFlyDAO.findAll();
+
+		List<PrivateFlyModel> aircrafts = privateflyService.getAllAircrafts();
+		logger.info("Listed all aircrafts");
 		map.addAttribute("aircrafts", aircrafts);
 		return "index";
 	}
@@ -39,31 +34,23 @@ public class HomeController {
 	public String registerPost(@RequestParam("aircraft_name") String aircraft_name,
 			@RequestParam("airfield") String airfield, @RequestParam("ICAO_code") String ICAO_code,
 			@RequestParam("openedDate") String openedDate, @RequestParam("runway_length") String runway_length) {
-		System.out.println(openedDate);
-		DateFormat df = new SimpleDateFormat("yyyy-mm-dd");
-		try {
-			Date startDate = df.parse(openedDate);
-			System.out.println(startDate);
-
-			PrivateFlyModel aircraft = new PrivateFlyModel(aircraft_name, airfield, ICAO_code, startDate,
-					runway_length);
-			privateFlyDAO.save(aircraft);
-		} catch (ParseException e) {
-			e.printStackTrace();
-		}
+		privateflyService.createNewAircraft(aircraft_name, airfield, ICAO_code, openedDate, runway_length);
+		logger.info("Created new aircraft");
 		return "redirect:/";
 	}
 
 	@RequestMapping("/sorted-list")
 	public String sortedAircraft(ModelMap map) {
-		List<PrivateFlyModel> aircrafts = privateFlyDAO.findAllByOrderByAirfieldAsc();
+		List<PrivateFlyModel> aircrafts = privateflyService.sortedAircraftList();
 		map.addAttribute("aircrafts", aircrafts);
+		logger.info("Get all aircrafts in ascending order");
 		return "index";
 	}
 
 	@RequestMapping(value = "/search-by-aircraft", method = RequestMethod.GET)
 	public String searchAircraft(ModelMap map, @RequestParam("airfield") String airfield) {
-		map.addAttribute("aircrafts", privateFlyDAO.findByAircraftname(airfield));
+		map.addAttribute("aircrafts", privateflyService.searchByAirfieldName(airfield));
+		logger.info("Search by airfield name");
 		return "index";
 	}
 }
--- src/main/java/com/privatefly/service/PrivateflyService.java
+++ src/main/java/com/privatefly/service/PrivateflyService.java
@@ -1,5 +1,8 @@
 package com.privatefly.service;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
@@ -7,12 +10,44 @@ import java.util.Date;
 import java.util.Iterator;
 import java.util.List;
 
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.privatefly.controller.HomeController;
+import com.privatefly.model.PrivateFlyDAO;
 import com.privatefly.model.PrivateFlyModel;
 
 @Service
 public class PrivateflyService {
-//   
 
+	private static final Logger logger = Logger.getLogger(HomeController.class);
+	
+	@Autowired
+	private PrivateFlyDAO privateFlyDAO;
+	
+	public List<PrivateFlyModel> getAllAircrafts(){
+		return privateFlyDAO.findAll();
+	}
+
+	public void createNewAircraft(String aircraftname, String airfield, String ICAO_code, String openedDate,
+		String runwayLength){
+		DateFormat df = new SimpleDateFormat("yyyy-mm-dd");
+		try {
+			Date startDate = df.parse(openedDate);;
+			PrivateFlyModel aircraft = new PrivateFlyModel(aircraftname, airfield, ICAO_code, startDate,
+					runwayLength);
+			privateFlyDAO.save(aircraft);
+		} catch (ParseException e) {
+			logger.info(e);
+		}
+	}
+	public List<PrivateFlyModel> sortedAircraftList(){
+		return privateFlyDAO.findAllByOrderByAirfieldAsc();
+	}
+	public PrivateFlyModel searchByAirfieldName(String airfield){
+		return privateFlyDAO.findByAircraftname(airfield);
+		
+	}
+	
 }
